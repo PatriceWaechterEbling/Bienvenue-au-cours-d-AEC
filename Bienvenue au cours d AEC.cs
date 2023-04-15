e1286c70496306603b5ede1e0309521721a61103
Patrice André Waechter-Ebling <97856916+PatriceWaechterEbling@users.noreply.github.com>
2023-04-15 09:53:36 -0400

    Add files via upload

---
+++ Bienvenue au cours d AEC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1_jour1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Bienvenue au cours d AEC";
+            Console.ForegroundColor = ConsoleColor.Cyan | ConsoleColor.DarkGray;
+            Console.WriteLine("Bon matin Patrice!\n\tBienvenue au cours d AEC");
+        }
+    }
+}
